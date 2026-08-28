34c81a6a615b0c2cd971b0d3c942c2b13e3815b0
venven1212 <abdelrahman.venve@gmail.com>
2026-08-28 15:16:42 +0400

    Add root endpoint to check API status

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => res.json({ status: 'Snaple API is running' }));
+
 app.use('/api/auth', authRoutes);
 app.use('/api/friends', friendRoutes);
 app.use('/api/chats', chatRoutes);
